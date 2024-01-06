e3dc0374e4606dd389b02e1874d1b9f37c8eff3e
wizard2122 <jackovlev.ilia2018@yandex.ru>
2024-01-06 12:51:02 +0300

    FirstLessonComplete

--- Assets/RagdollProject/Scripts/Example1/FirstExample.cs
+++ Assets/RagdollProject/Scripts/Example1/FirstExample.cs
@@ -4,6 +4,11 @@ public class FirstExample : MonoBehaviour
 {
     [SerializeField] private Enemy _enemy;
 
+    private void Awake()
+    {
+        _enemy.Initialize();
+    }
+
     private void Update()
     {
         if (Input.GetKeyUp(KeyCode.F))
